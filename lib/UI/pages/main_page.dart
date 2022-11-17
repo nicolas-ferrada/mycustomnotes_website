2c70eae006c181209583e0f8699a01cb97712c6b
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2022-11-17 18:07:51 -0300

    small color changes

--- lib/UI/pages/main_page.dart
+++ lib/UI/pages/main_page.dart
@@ -14,10 +14,11 @@ class MainPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Login button
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 textStyle: const TextStyle(fontSize: 30),
-                backgroundColor: Colors.red,
+                backgroundColor: const Color.fromARGB(255, 221, 86, 76),
                 minimumSize: const Size(250, 100),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
@@ -27,6 +28,33 @@ class MainPage extends StatelessWidget {
               onPressed: () {},
               child: const Text('Login'),
             ),
+            const Text(
+              'Press to log in to your account',
+              style: TextStyle(color: Color.fromARGB(255, 199, 199, 199)),
+            ),
+            const Divider(height: 80),
+            //Register button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 30),
+                backgroundColor: const Color.fromARGB(255, 221, 86, 76),
+                minimumSize: const Size(250, 100),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.all(10),
+              ),
+              onPressed: () {},
+              child: const Text('Register'),
+            ),
+            const Text(
+              'Press to create an account',
+              style: TextStyle(
+                color: Color.fromARGB(255, 199, 199, 199),
+              ),
+            ),
+            //Exit button
+            const Padding(padding: EdgeInsets.all(20)),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.redAccent,
