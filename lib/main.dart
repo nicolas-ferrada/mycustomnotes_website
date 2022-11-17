03388dbbf32186c79a8bdf143195ba5f4d55575f
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2022-11-17 11:52:43 -0300

    test branch message

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo test branch',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
