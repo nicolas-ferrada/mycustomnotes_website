25a3bb05999344eb4be82ee74a8b66010d590249
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2022-11-18 18:29:44 -0300

    register page finish ui

--- lib/UI/pages/register_page.dart
+++ lib/UI/pages/register_page.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatefulWidget {
+class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
-  State<RegisterPage> createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends State<RegisterPage> {
-  @override
   Widget build(BuildContext context) {
-        return const Text(
-      'Register page'
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Register'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Mail user input
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            //password user input
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Repeat your password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            //Login button
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 30),
+                  backgroundColor: const Color.fromARGB(255, 221, 86, 76),
+                  minimumSize: const Size(220, 70),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                ),
+                onPressed: () {},
+                child: const Text('Register new user'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
