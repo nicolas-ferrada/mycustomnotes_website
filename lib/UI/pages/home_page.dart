8a87aa661054d6fd2f84441c7bd9207f4da6d6ac
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2022-12-09 20:55:44 -0300

    current status

--- lib/UI/pages/home_page.dart
+++ lib/UI/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "You're logged in, welcome to home page, user: "
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Signed in as:",
+            style: TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '${user.email}',
+            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.redAccent,
+            ),
+            onPressed: () {
+              _logoutFirebase();
+            },
+            icon: const Icon(Icons.arrow_back),
+            label: const Text(
+              'Sign out',
+              style: TextStyle(fontSize: 24),
+            ),
+          )
+        ],
+      ),
     );
   }
+}
+
+Future _logoutFirebase() async {
+  await FirebaseAuth.instance.signOut();
 }
--- lib/UI/pages/login_page.dart
+++ lib/UI/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mycustomnotes/UI/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -26,75 +27,84 @@ class _LoginPageState extends State<LoginPage> {
         centerTitle: true,
         title: const Text('My Custom Notes'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Mail user input
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: _emailLoginController,
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [AutofillHints.email],
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email',
-                  prefixIcon: Icon(Icons.mail),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            //Password user input
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _passwordLoginController,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            //Login button
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                icon: const Icon(
-                  Icons.lock_open,
-                  size: 32,
-                ),
-                label: const Text(
-                  'Login',
-                  style: TextStyle(fontSize: 30),
-                ),
-                style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 30),
-                  backgroundColor: const Color.fromARGB(255, 221, 86, 76),
-                  minimumSize: const Size(220, 70),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const HomePage();
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Mail user input
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _emailLoginController,
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: const [AutofillHints.email],
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your email',
+                        prefixIcon: Icon(Icons.mail),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                  padding: const EdgeInsets.all(10),
-                ),
-                onPressed: () {
-                  _loginFirebase(_emailLoginController.text, _passwordLoginController.text);
-                },
+                  //Password user input
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _passwordLoginController,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  //Login button
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.lock_open,
+                        size: 32,
+                      ),
+                      label: const Text(
+                        'Login',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 30),
+                        backgroundColor: const Color.fromARGB(255, 221, 86, 76),
+                        minimumSize: const Size(220, 70),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                      ),
+                      onPressed: () {
+                        _loginFirebase(_emailLoginController.text,
+                            _passwordLoginController.text);
+                      },
+                    ),
+                  ),
+                  //Press here to create an account button
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/RegisterPage');
+                    },
+                    child: const Text('Press here to create an account',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 160, 160, 160))),
+                  ),
+                ],
               ),
-            ),
-            //Press here to create an account button
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/RegisterPage');
-              },
-              child: const Text('Press here to create an account',
-                  style: TextStyle(color: Color.fromARGB(255, 160, 160, 160))),
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
