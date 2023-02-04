ed2a683ca6f07e19e90204becedc91f3cbddb9b4
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-02-04 15:16:32 -0300

    read all notes from firebase

--- lib/database/firebase/cloud_database_helper.dart
+++ lib/database/firebase/cloud_database_helper.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'dart:developer' as log;
 import '../../models/note_model.dart';
 
 class CloudDatabaseHelper {
+
   // Create a note in firebase
   static Future<void> createNoteCloudDB(Note note) async {
     // References to the firestore colletion.
@@ -13,5 +14,24 @@ class CloudDatabaseHelper {
 
     // Store the Note map in firestore
     await docUser.add(mapNote);
+  }
+
+  // Read all notes
+  static Stream<List<Note>> readAllNotesCloudDB() async* {
+    try {
+      final db = FirebaseFirestore.instance;
+      final collectionNote = db.collection('note');
+      final snapshot = await collectionNote.get();
+      List<Note> notes = [];
+      for (QueryDocumentSnapshot<Map<String, dynamic>> docSnapshots
+          in snapshot.docs) {
+        final data = Note.fromMap(docSnapshots.data());
+        log.log(data.toString());
+        notes.add(data);
+      }
+      yield notes;
+    } catch (e) {
+      throw Exception('Error reading all notes: $e');
+    }
   }
 }
