0829b601308820c55288e6b431106ab27514f123
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-02-10 20:14:14 -0300

    date formatted more understandable (bug star position)

--- lib/utils/formatters/date_formatter.dart
+++ lib/utils/formatters/date_formatter.dart
@@ -4,16 +4,48 @@ class DateFormatter {
 // Formate the timestamp from firebase to a user friendly date
 // Day, Month, Year, Hour, Minutes
   static String showDateFormatted(Timestamp lastModificationDate) {
-    final DateTime dateTime = lastModificationDate.toDate();
-    //final String formattedTime = DateFormat('d-M-y H:m').format(dateTime);
+    final DateTime dateTimeFromDatabase = lastModificationDate.toDate();
+    String monthName = DateFormatter.getMonthName(dateTimeFromDatabase);
+    final today = DateTime.now();
+    late String formattedTime;
+    String formattedHour = dateTimeFromDatabase.hour.toString().padLeft(2, '0');
+    String formattedMinutes =
+        dateTimeFromDatabase.minute.toString().padLeft(2, '0');
+    String formattedDay = dateTimeFromDatabase.day.toString().padLeft(2, '0');
 
-    final day = dateTime.day.toString().padLeft(2, '0');
-    final month = dateTime.month.toString().padLeft(2, '0');
-    final hour = dateTime.hour.toString().padLeft(2, '0');
-    final minute = dateTime.minute.toString().padLeft(2, '0');
-
-    final String formattedTime = '$day-$month-${dateTime.year} $hour:$minute';
-
+    // Formatted date if the day is today
+    if (dateTimeFromDatabase.day == today.day &&
+        dateTimeFromDatabase.month == today.month &&
+        dateTimeFromDatabase.year == today.year) {
+      formattedTime = '$formattedHour:$formattedMinutes';
+      // Formatted date if the date is from the current year
+    } else if (dateTimeFromDatabase.year == DateTime.now().year) {
+      formattedTime = '$formattedDay $monthName';
+    } else {
+      // If is not today and not this year, show the year of the note.
+      formattedTime = '$formattedDay $monthName ${dateTimeFromDatabase.year}';
+    }
     return formattedTime;
+  }
+
+  // Get the month of the note in
+  static String getMonthName(DateTime date) {
+    // final spanishMonth = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    final englishMonths = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    final index = date.month - 1;
+    return englishMonths[index];
   }
 }
