6b8827f6006ed34edd0e8dc72bd61bc640ed617e
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-03-21 16:47:13 -0300

    select note type
    alert dialog to select note type on creation style modification

--- lib/utils/dialogs/select_create_note_type.dart
+++ lib/utils/dialogs/select_create_note_type.dart
@@ -9,32 +9,86 @@ class SelectCreateNoteType {
       builder: (BuildContext context) {
         return AlertDialog(
           elevation: 3,
-          backgroundColor: const Color.fromRGBO(250, 216, 90, 0.9),
-          title: const Center(
-            child: Text('Note type'),
+          backgroundColor: const Color.fromRGBO(250, 216, 90, 0.8),
+          title: Center(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              color: Colors.grey.shade800.withOpacity(0.9),
+              child: const Text(
+                'Note type',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
           content: SizedBox(
             child: Row(
               children: [
                 Flexible(
                   child: Card(
-                    child: ListTile(
-                      title: const Text('Text note'),
+                    color: Colors.grey.shade800.withOpacity(0.9),
+                    child: InkWell(
                       onTap: () {
                         Navigator.pop(context);
                         Navigator.pushNamed(context, noteTextCreatePageRoute);
                       },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          SizedBox(height: 8),
+                          Icon(
+                            Icons.text_snippet,
+                            size: 46,
+                            color: Colors.white,
+                          ),
+                          SizedBox(height: 8),
+                          Padding(
+                            padding: EdgeInsets.all(12),
+                            child: Text(
+                              'Text Note',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
                 Flexible(
                   child: Card(
-                    child: ListTile(
-                      title: const Text('Tasks note'),
+                    color:
+                        Colors.grey.shade800.withOpacity(0.9),
+                    child: InkWell(
                       onTap: () {
                         Navigator.pop(context);
                         Navigator.pushNamed(context, noteTasksCreatePageRoute);
                       },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          SizedBox(height: 8),
+                          Icon(
+                            Icons.check_box,
+                            size: 46,
+                            color: Colors.white,
+                          ),
+                          SizedBox(height: 8),
+                          Padding(
+                            padding: EdgeInsets.all(12),
+                            child: Text(
+                              'Tasks Note',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -49,13 +103,16 @@ class SelectCreateNoteType {
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         minimumSize: const Size(200, 40),
-                        backgroundColor: Colors.white),
+                        backgroundColor: Colors.grey.shade800.withOpacity(0.9)),
                     onPressed: () {
                       Navigator.maybePop(context);
                     },
                     child: const Text(
                       'Cancel note creation',
-                      style: TextStyle(color: Colors.black),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                   const SizedBox(
