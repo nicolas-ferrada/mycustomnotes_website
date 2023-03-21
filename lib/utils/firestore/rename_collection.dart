0c9f4c09375d0532dcfee11394d91eb24e94dde3
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-03-20 21:32:38 -0300

    rename collection firestore naming fix

--- lib/utils/firestore/rename_collection.dart
+++ lib/utils/firestore/rename_collection.dart
@@ -1,19 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreUtils {
-  static Future<void> renameCollection(String note, String noteText) async {
+  static Future<void> renameCollection(
+    String currentName,
+    String newName,
+  ) async {
     try {
       final db = FirebaseFirestore.instance;
 
       // 1. Create a new collection with the desired name.
-      await db.collection(noteText).doc('dummy').set({});
+      await db.collection(newName).doc('dummy').set({});
 
       // 2. Read all the documents from the old collection.
-      final oldCollectionRef = db.collection(note);
+      final oldCollectionRef = db.collection(currentName);
       final oldCollectionDocs = await oldCollectionRef.get();
 
       // 3. Write the documents to the new collection.
-      final newCollectionRef = db.collection(noteText);
+      final newCollectionRef = db.collection(newName);
       final batch = db.batch();
       for (final doc in oldCollectionDocs.docs) {
         batch.set(newCollectionRef.doc(doc.id), doc.data());
@@ -24,7 +27,7 @@ class FirestoreUtils {
       for (final doc in oldCollectionDocs.docs) {
         await oldCollectionRef.doc(doc.id).delete();
       }
-      await db.collection(note).doc('dummy').delete();
+      await db.collection(currentName).doc('dummy').delete();
     } catch (e) {
       throw Exception('Error renaming collection: $e');
     }
