71467b716f7f27840db660ce68d9817c4f83d40e
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-05-03 14:00:20 -0400

    removed unnecesary code

--- lib/presentation/pages/home_page/home_page_widgets/home_page_navigation_drawer.dart
+++ lib/presentation/pages/home_page/home_page_widgets/home_page_navigation_drawer.dart
@@ -8,7 +8,6 @@ import 'package:mycustomnotes/utils/enums/select_language_enum.dart';
 import '../../../../utils/dialogs/confirmation_dialog.dart';
 import '../../../../utils/dialogs/user_configuration_dialogs/user_configuration_language.dart';
 import '../../../../utils/enums/notes_view_enum.dart';
-import 'dart:developer' as log;
 
 class NavigationDrawerHomePage extends StatelessWidget {
   final User currentUser;
@@ -107,7 +106,6 @@ class NavigationDrawerHomePage extends StatelessWidget {
   }) {
     return ListTile(
       onTap: () async {
-        log.log(userConfiguration.dateTimeFormat);
         final String? dateTimeFormat = await showDialog<String?>(
           context: context,
           builder: (context) {
