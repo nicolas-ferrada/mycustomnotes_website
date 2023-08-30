a5eddda57f20465ac5c73847ae0a5ae792a79894
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-08-30 18:01:10 -0400

    UI Changes
    - Login screen updated
    - Added a color scheme to the app

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -6,6 +6,7 @@ import '../../../domain/services/auth_user_service.dart';
 import '../../../l10n/change_language.dart';
 import '../../../l10n/l10n_export.dart';
 import '../../../l10n/l10n_locale_provider.dart';
+import '../../../utils/app_color_scheme/app_color_scheme.dart';
 import '../../../utils/dialogs/change_language_dialog.dart';
 import '../../../utils/enums/select_language_enum.dart';
 import '../../../utils/exceptions/exceptions_alert_dialog.dart';
@@ -60,181 +61,273 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           physics: const ClampingScrollPhysics(),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Change language
-              InkWell(
-                borderRadius: BorderRadius.circular(20),
-                onTap: () async {
-                  try {
-                    final SelectLanguage? language =
-                        await showDialog<SelectLanguage?>(
-                      context: context,
-                      builder: (context) {
-                        return ChangeLanguageDialog(
-                          context: context,
-                        );
-                      },
-                    );
-                    if (language != null && context.mounted) {
-                      await ChangeLanguage.changeLanguage(
-                          context: context, language: language.lenguageId);
-                    }
-                  } catch (errorMessage) {
-                    // errorMessage is the custom message probably sent by the user configuration functions
-                    ExceptionsAlertDialog.showErrorDialog(
-                        context: context,
-                        errorMessage: errorMessage.toString());
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    language,
-                    style: const TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Colors.white70,
-                      fontSize: 16,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 64,
-              ),
-              //Mail user input
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onTapOutside: (event) => FocusScope.of(context).unfocus(),
-                  controller: _emailLoginController,
-                  keyboardType: TextInputType.emailAddress,
-                  autofillHints: const [AutofillHints.email],
-                  decoration: InputDecoration(
-                    hintText: AppLocalizations.of(context)!
-                        .email_textformfield_loginPage,
-                    prefixIcon: const Icon(Icons.mail),
-                    border: const OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              //Password user input
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  onTapOutside: (event) => FocusScope.of(context).unfocus(),
-                  controller: _passwordLoginController,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    hintText: AppLocalizations.of(context)!
-                        .password_textformfield_loginPage,
-                    prefixIcon: const Icon(Icons.lock),
-                    border: const OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              //Login button
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton.icon(
-                  icon: const Icon(
-                    Icons.lock_open,
-                    size: 32,
-                  ),
-                  label: Text(
-                    AppLocalizations.of(context)!.login_button_loginPage,
-                    style: const TextStyle(fontSize: 30),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 30),
-                    backgroundColor: const Color.fromRGBO(250, 216, 90, 0.9),
-                    minimumSize: const Size(220, 70),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12, vertical: 24),
-                  ),
-                  onPressed: () async {
-                    // Login firebase.
-                    try {
-                      await AuthUserService.loginUserFirebase(
-                        email: _emailLoginController.text,
-                        password: _passwordLoginController.text,
-                        context: context,
-                      );
-                    } catch (errorMessage) {
-                      // errorMessage is the custom message sent by the firebase function.
-                      ExceptionsAlertDialog.showErrorDialog(
-                          context: context,
-                          errorMessage: errorMessage.toString());
-                    }
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              //Rich text 'need an account?' Sign up
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RichText(
-                  text: TextSpan(
-                      style: const TextStyle(color: Colors.grey, fontSize: 18),
-                      text: AppLocalizations.of(context)!
-                          .signUp_text_info_loginPage,
-                      children: [
-                        TextSpan(
-                            style: const TextStyle(
-                                decoration: TextDecoration.underline,
-                                color: Colors.white),
-                            text: AppLocalizations.of(context)!
-                                .signUp_richText_loginPage,
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.pushNamed(context, registerPageRoute);
-                              }),
-                      ]),
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              //Rich text text Forgot your password? Recover it
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RichText(
-                  text: TextSpan(
-                    style: const TextStyle(color: Colors.grey, fontSize: 18),
-                    text: AppLocalizations.of(context)!
-                        .passwordRecover_text_info_loginPage,
-                    children: [
-                      TextSpan(
-                        style: const TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Colors.white),
-                        text: AppLocalizations.of(context)!
-                            .passwordRecover_richText_loginPage,
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.pushNamed(
-                                context, recoverPasswordPageRoute);
-                          },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 96,
-              ),
+              changeLanguageWidget(language, context),
+              emailUserInputWidget(context),
+              passwordUserInputWidget(context),
+              loginButtonWidget(context),
+              forgotPasswordWidget(context),
+              loginProvidersSeparationWidget(context),
+              googleLoginButtonWidget(context),
+              createAccountSeparationWidget(context),
+              signUpWidget(context),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget changeLanguageWidget(String language, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        onTap: () async {
+          try {
+            final SelectLanguage? language = await showDialog<SelectLanguage?>(
+              context: context,
+              builder: (context) {
+                return ChangeLanguageDialog(
+                  context: context,
+                );
+              },
+            );
+            if (language != null && context.mounted) {
+              await ChangeLanguage.changeLanguage(
+                  context: context, language: language.lenguageId);
+            }
+          } catch (errorMessage) {
+            // errorMessage is the custom message probably sent by the user configuration functions
+            ExceptionsAlertDialog.showErrorDialog(
+                context: context, errorMessage: errorMessage.toString());
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            language,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget emailUserInputWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: TextFormField(
+        onTapOutside: (event) => FocusScope.of(context).unfocus(),
+        controller: _emailLoginController,
+        keyboardType: TextInputType.emailAddress,
+        autofillHints: const [AutofillHints.email],
+        decoration: InputDecoration(
+          hintText: AppLocalizations.of(context)!.email_textformfield_loginPage,
+          prefixIcon: const Icon(Icons.mail),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColorScheme.purple()),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey.shade600),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget passwordUserInputWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: TextField(
+        onTapOutside: (event) => FocusScope.of(context).unfocus(),
+        controller: _passwordLoginController,
+        obscureText: true,
+        enableSuggestions: false,
+        autocorrect: false,
+        decoration: InputDecoration(
+          hintText:
+              AppLocalizations.of(context)!.password_textformfield_loginPage,
+          prefixIcon: const Icon(Icons.lock),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColorScheme.purple()),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey.shade600),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginButtonWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 30,
+          backgroundColor: AppColorScheme.darkPurple(),
+          minimumSize: const Size(200, 60),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        onPressed: () async {
+          // Login firebase.
+          try {
+            await AuthUserService.loginUserFirebase(
+              email: _emailLoginController.text,
+              password: _passwordLoginController.text,
+              context: context,
+            );
+          } catch (errorMessage) {
+            // errorMessage is the custom message sent by the firebase function.
+            ExceptionsAlertDialog.showErrorDialog(
+                context: context, errorMessage: errorMessage.toString());
+          }
+        },
+        child: Text(
+          AppLocalizations.of(context)!.login_button_loginPage,
+          style: const TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget forgotPasswordWidget(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, recoverPasswordPageRoute);
+      },
+      borderRadius: BorderRadius.circular(20),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          AppLocalizations.of(context)!.passwordRecover_richText_info_loginPage,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginProvidersSeparationWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Divider(
+              thickness: 0.5,
+              color: Colors.grey.shade600,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text('Or'),
+          ),
+          Expanded(
+            child: Divider(
+              thickness: 0.5,
+              color: Colors.grey.shade600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget googleLoginButtonWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 10,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+        onPressed: () {},
+        child: const Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 22),
+              child: Image(
+                image: AssetImage("assets/google_icon.png"),
+                height: 22,
+                width: 22,
+              ),
+            ),
+            Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black54,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createAccountSeparationWidget(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: Divider(
+        thickness: 1,
+        color: Colors.white70,
+      ),
+    );
+  }
+
+  Widget signUpWidget(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          AppLocalizations.of(context)!.signUp_text_info_loginPage,
+          style: const TextStyle(
+            color: Colors.white70,
+            fontSize: 16,
+          ),
+        ),
+        InkWell(
+          borderRadius: BorderRadius.circular(20),
+          onTap: () => Navigator.pushNamed(context, registerPageRoute),
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: Text(
+              AppLocalizations.of(context)!.signUp_richText_loginPage,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 19,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/utils/app_color_scheme/app_color_scheme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColorScheme {
+  static Color raisinBlack() {
+    return const Color.fromRGBO(33, 30, 41, 1);
+  }
+
+  static Color darkPurple() {
+    return const Color.fromRGBO(50, 45, 64, 1);
+  }
+
+  static Color purple() {
+    return const Color.fromRGBO(111, 96, 190, 1);
+  }
+}
