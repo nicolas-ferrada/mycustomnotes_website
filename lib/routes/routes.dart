dc5639f70110b60d1bcd72f73779ae2a2fe03af5
nicolas-ferrada <nicofrrd@gmail.com>
2023-09-28 13:14:01 -0300

    Refactor remove unused import

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,3 @@
-// import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mycustomnotes_website/pages/contact_page.dart';
 import 'package:mycustomnotes_website/pages/home_page.dart';
