2a6b4959f6f5f5a78c1951c38acacc524c89c553
nicolas-ferrada <nicofrrd@gmail.com>
2023-09-29 18:27:14 -0300

    Add Privacy Policy

--- lib/pages/privacy_policy_page.dart
+++ lib/pages/privacy_policy_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PrivacyPolicyPage extends StatelessWidget {
   const PrivacyPolicyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Privacy Policy'),
+    return FutureBuilder<String>(
+      future: rootBundle.loadString("assets/privacy-policy.md"),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.hasData) {
+          return Markdown(
+            data: snapshot.data!,
+            styleSheet: markdownStyle(),
+            selectable: true,
+            padding: const EdgeInsets.symmetric(horizontal: 500, vertical: 64),
+            onTapLink: (text, url, title) {
+              if (url != null) {
+                launchUrl(Uri.parse(url));
+              }
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  MarkdownStyleSheet markdownStyle() {
+    return MarkdownStyleSheet(
+      // All text
+      p: const TextStyle(fontSize: 18),
+
+      // All text spacing
+      blockSpacing: 16,
+
+      // H1: first title
+      h1: const TextStyle(fontSize: 50),
+      h1Padding: const EdgeInsets.only(bottom: 32),
+      h1Align: WrapAlignment.center,
+
+      // H2: block title
+      h2: const TextStyle(fontSize: 38),
+      h2Padding: const EdgeInsets.only(top: 64),
+
+      // H3: block subtitle
+      h3: const TextStyle(fontSize: 28),
+      h3Padding: const EdgeInsets.only(top: 12),
     );
   }
 }
