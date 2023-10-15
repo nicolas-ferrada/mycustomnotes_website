3db2b957078652562c5198712a081f3ffd34154a
nicolas-ferrada <nicofrrd@gmail.com>
2023-10-14 22:36:29 -0300

    Add MVP contact page

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -5,8 +5,75 @@ class ContactPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Contact'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        titlePage(),
+        bodyPage(),
+      ],
+    );
+  }
+
+  Widget bodyPage() {
+    return const Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Contact information',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            'Owner and data controller',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            'Name: Nicolás Ferrada',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          SelectableText(
+            'Email: mycustomnotes@nicolasferrada.com',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'If you have any questions, you can contact us by email.',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget titlePage() {
+    return const Padding(
+      padding: EdgeInsets.all(12),
+      child: Expanded(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Text(
+            'Contact us',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
