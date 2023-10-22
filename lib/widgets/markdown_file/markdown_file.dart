0219053098b7bc57334647ea0188b7774ec26676
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2023-10-22 20:47:05 -0300

    feat markdown files responsive

---
+++ lib/widgets/markdown_file/markdown_file.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mycustomnotes_website/widgets/markdown_file/markdown_file_widgets/markdown_file_document.dart';
+
+class MarkdownFile extends StatelessWidget {
+  final String fileLocationName;
+  const MarkdownFile({
+    super.key,
+    required this.fileLocationName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: rootBundle.loadString(fileLocationName),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.hasData) {
+          return LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              if (constraints.maxWidth < 700) {
+                return mobileMarkdownFile(snapshot: snapshot);
+              } else if (constraints.maxWidth < 1500) {
+                return tabletMarkdownFile(snapshot: snapshot);
+              } else {
+                return desktopMarkdownFile(snapshot: snapshot);
+              }
+            },
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return Center(
+            child: Text('An error ocurred with the file: ${snapshot.error}'),
+          );
+        }
+      },
+    );
+  }
+
+  Widget mobileMarkdownFile({
+    required AsyncSnapshot snapshot,
+  }) {
+    return MarkdownFileDocument(
+      snapshot: snapshot,
+      horizontalPadding: 40,
+    );
+  }
+
+  Widget tabletMarkdownFile({
+    required AsyncSnapshot snapshot,
+  }) {
+    return MarkdownFileDocument(
+      snapshot: snapshot,
+      horizontalPadding: 150,
+    );
+  }
+
+  Widget desktopMarkdownFile({
+    required AsyncSnapshot snapshot,
+  }) {
+    return MarkdownFileDocument(
+      snapshot: snapshot,
+      horizontalPadding: 400,
+    );
+  }
+}
